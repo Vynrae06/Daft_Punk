9f716330c5aebe5916b691672c4deaf07df8c530
Vynrae <ghaith.elghaddaf@gmail.com>
2023-01-26 20:07:59 +0100

    V1

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject followObject;
+    public Vector2 followOffset;
+    private Vector2 threshold;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        threshold = CalculateThreshold();
+        speed = followObject.GetComponent<Movement>().speed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        Vector2 follow = followObject.transform.position;
+        float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+        float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+
+        Vector3 newPosition = transform.position;
+        if (Mathf.Abs(xDifference) >= threshold.x)
+        {
+            newPosition.x = follow.x;
+        }
+        if (Mathf.Abs(yDifference) >= threshold.y)
+        {
+            newPosition.y = follow.y;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+    }
+
+    private Vector3 CalculateThreshold()
+    {
+        Rect aspect = Camera.main.pixelRect;
+        Vector2 calculatedThreshold = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+        calculatedThreshold.x -= followOffset.x;
+        calculatedThreshold.y -= followOffset.y;
+        return calculatedThreshold;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector2 border = CalculateThreshold();
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+    }
+}
